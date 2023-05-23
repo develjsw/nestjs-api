9ab54ec715757e6eeaba5d1ebe6f8cc6209d5df6
jsw <develjsw@gmail.com>
2023-05-23 23:41:23 +0900

    TypeOrm 설정(동기식 → 비동기식)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configurationLocal from './config/configuration.local';
 import configurationDevelopment from './config/configuration.development';
 import configurationProduction from './config/configuration.production';
@@ -26,18 +26,12 @@ if (process.env.NODE_ENV === 'production') {
             configuration
         ]
       }),
-      // TODO : forRoot → forRootAsync 교체 예정
-      TypeOrmModule.forRoot({
-          type: 'mysql',
-          host: process.env.DATABASE_HOST,
-          port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
-          username: process.env.DATABASE_USERNAME,
-          password: process.env.DATABASE_PASSWORD,
-          database: process.env.DATABASE_SCHEMA,
-          entities: [__dirname + '/**/*.entity{.ts,.js}'], // entity 인식
-          synchronize: false,
-          logging: 'all'
-      })
+      TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: (configService: ConfigService) =>
+              configService.get('database'),
+          inject: [ConfigService]
+      }),
   ],
   /* controller */
   controllers: [AppController],
--- src/config/configuration.local.ts
+++ src/config/configuration.local.ts
@@ -1,4 +1,14 @@
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000, // .env 파일의 PORT 값을 가져오되 10진수로 변환해서 가져옴, 디폴트 값 = 3000
-    // TODO : 설정 값 추가 예정
+    database: {
+        type: process.env.DATABASE_TYPE,
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_SCHEMA,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'], // entity 인식
+        synchronize: false,
+        logging: 'all'
+    }
 })
