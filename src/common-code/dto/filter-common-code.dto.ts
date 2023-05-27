0b8f83207da072512df665743964a5e1996e94ca
jsw <develjsw@gmail.com>
2023-05-28 01:57:31 +0900

    공통 코드 모듈 작업 (validation 적용, entity 관계 설정 추가)

---
+++ src/common-code/dto/filter-common-code.dto.ts
@@ -0,0 +1,13 @@
+import { IsOptional, IsString, Length } from 'class-validator';
+
+export class FilterCommonCodeDto {
+    @IsOptional()
+    @IsString()
+    @Length(2,3)
+    mainCd: string;
+
+    @IsOptional()
+    @IsString()
+    @Length(2,3)
+    subCd: string;
+}
