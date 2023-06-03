5300d9f3b9291db12bdeab391776dbcae735fcb2
jsw <develjsw@gmail.com>
2023-06-03 17:09:53 +0900

    all exception filter(http exception)에 DB exception 추가

--- src/common/exception/all-exception/all-exception.filter.ts
+++ src/common/exception/all-exception/all-exception.filter.ts
@@ -6,30 +6,32 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
+import { DBException } from '../db-exception/db-exception';
 
-@Catch()
+@Catch(HttpException, DBException)
 export class AllExceptionFilter<T> implements ExceptionFilter {
   constructor(
       private readonly httpAdapterHost: HttpAdapterHost
   ) {}
 
-  catch(exception: unknown, host: ArgumentsHost): void {
+  catch(exception: HttpException | DBException, host: ArgumentsHost): void {
 
     const { httpAdapter } = this.httpAdapterHost;
     const ctx = host.switchToHttp();
 
-    const httpStatus =
-        exception instanceof HttpException
-            ? exception.getStatus() // code 값 ex) 404 ...
-            : HttpStatus.INTERNAL_SERVER_ERROR; // 500 Error
+    const response = ctx.getResponse();
+    const status = exception.getStatus();
+    const message = exception.message;
 
-    // TODO : timestamp, path 제거 예정 / desc, message 추가 예정
     const responseBody = {
-      statusCode: httpStatus,
-      timestamp: new Date().toISOString(),
-      path: httpAdapter.getRequestUrl(ctx.getRequest()), // 요청 url path 정보 ex) /member/create123456789
+      status: status,
+      message: message
     };
 
-    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+    httpAdapter.reply(
+      response,
+      responseBody
+    );
+
   }
 }
