7b7547336b24cda27997ae25dc2819d399826a84
jsw <develjsw@gmail.com>
2023-06-23 23:12:52 +0900

    공통 코드 그룹핑(lodash 사용) 리스트 추가

--- src/common-code/common-code.module.ts
+++ src/common-code/common-code.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { CommonCodeService } from './common-code.service';
 import { CommonCodeController } from './common-code.controller';
 import { CommonCodeMain } from './entities/common-code-main.entity';
+import { CommonCodeSub } from './entities/common-code-sub.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
-      CommonCodeMain
+      CommonCodeMain,
+      CommonCodeSub
     ])
   ],
   controllers: [CommonCodeController],
