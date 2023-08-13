33b45bc0d118cefc904f9e62ef7faca4dfb88ac6
jsw <develjsw@gmail.com>
2023-08-13 21:41:33 +0900

    (변경) 내장 캐시 → redis server 연결

--- src/common/cache/redis-cache.module.ts
+++ src/common/cache/redis-cache.module.ts
@@ -1,21 +1,25 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
-import * as redisStore from 'cache-manager-redis-store';
+import { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-yet';
 import { RedisCacheService } from './redis-cache.service';
 
 @Global()
 @Module({
   imports: [
-      CacheModule.register({
-          imports: [ConfigModule],
-          inject: [ConfigService],
-          useFactory: async (configService: ConfigService) => ({
-              store: redisStore,
-              host: configService.get('database.redis.host'),
-              port: configService.get('database.redis.port'),
-              ttl: configService.get('database.redis.defaultTTL')
-          })
+      CacheModule.registerAsync<RedisClientOptions>({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async(configService: ConfigService) => ({
+            store: await redisStore,
+            socket: {
+                host: configService.get('database.redis.host'),
+                port: configService.get('database.redis.port'),
+                // TODO : module에서 설정한 ttl 값이 적용되지 않는 이슈가 존재하여 사용하는 곳에서 option으로 설정 중
+                //ttl: configService.get('database.redis.defaultTTL')
+            }
+        }),
       })
   ],
   providers: [RedisCacheService],
