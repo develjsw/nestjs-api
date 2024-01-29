ca7ae35470c855d0ce81d1afbce9f36c907cc490
jsw <develjsw@gmail.com>
2024-01-29 22:27:29 +0900

    Service(business logic)와 Repository 분리

--- src/common-code/common-code.module.ts
+++ src/common-code/common-code.module.ts
@@ -5,18 +5,22 @@ import { CommonCodeMain } from './entities/common-code-main.entity';
 import { CommonCodeSub } from './entities/common-code-sub.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResponseService } from '../common/response/response.service';
+import { CommonCodeMainRepository } from './repository/common-code-main.repository';
+import { CommonCodeSubRepository } from './repository/common-code-sub.repository';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      CommonCodeMain,
-      CommonCodeSub
-    ])
-  ],
-  controllers: [CommonCodeController],
-  providers: [
-    ResponseService,
-    CommonCodeService,
-  ]
+    imports: [
+        TypeOrmModule.forFeature([
+            CommonCodeMain,
+            CommonCodeSub
+        ])
+    ],
+    controllers: [CommonCodeController],
+    providers: [
+        ResponseService,
+        CommonCodeService,
+        CommonCodeMainRepository,
+        CommonCodeSubRepository,
+    ]
 })
 export class CommonCodeModule {}
---
+++ src/common-code/repository/common-code-sub.repository.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { CommonCodeSub } from '../entities/common-code-sub.entity';
+import { CommonCodeMain } from '../entities/common-code-main.entity';
+import * as _ from 'lodash';
+
+@Injectable()
+export class CommonCodeSubRepository {
+    private commonCodeSubRepository: Repository<CommonCodeSub>
+
+    constructor(private readonly dataSource: DataSource) {
+        this.commonCodeSubRepository = this.dataSource.getRepository(CommonCodeSub)
+    }
+
+    async getAllListByGroup() {
+        const rawDataList = await this.commonCodeSubRepository
+            .createQueryBuilder('ccs')
+            .select([
+                'ccm.mainCd as mainCd',
+                'ccm.mainNm as mainNm',
+                'ccs.subCd as subCd',
+                'ccs.subNm as subNm',
+                'ccs.codeDesc as codeDesc',
+                'ccs.sortNo as sortNo',
+            ])
+            .leftJoin(CommonCodeMain, 'ccm', 'ccs.mainCd = ccm.mainCd')
+            .where('ccs.isUse = "Y"')
+            .andWhere('ccm.isUse = "Y"')
+            .andWhere('ccs.delDate IS NULL')
+            .andWhere('ccm.delDate IS NULL')
+            .orderBy('ccs.sortNo', 'ASC')
+            .execute()
+
+        return _.groupBy(rawDataList, 'mainCd')
+    }
+}
