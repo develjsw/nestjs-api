6fc34f40091b1a4c3fc297ed243f99f6b18062ec
jsw <develjsw@gmail.com>
2024-02-24 21:02:27 +0900

    prettier - printWidth : 80으로 설정 변경

--- src/common/exception/manager-exception.ts
+++ src/common/exception/manager-exception.ts
@@ -1,7 +1,11 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class ManagerException extends HttpException {
-    constructor(readonly code: number, message: string, httpStatus: number = HttpStatus.BAD_REQUEST) {
+    constructor(
+        readonly code: number,
+        message: string,
+        httpStatus: number = HttpStatus.BAD_REQUEST
+    ) {
         super(message, httpStatus);
     }
 }
