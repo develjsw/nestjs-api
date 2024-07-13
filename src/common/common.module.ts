6d3b3de41de069148ab34389b6c5d97c7121166a
jsw <develjsw@gmail.com>
2024-07-13 22:41:56 +0900

    kafka test

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { SlackModule } from './slack/slack.module';
 import { RedisCacheModule } from './cache/redis-cache.module';
+import { KafkaService } from './kafka/kafka.service';
 
 @Global()
 @Module({
-    providers: [],
-    exports: [],
+    providers: [KafkaService],
+    exports: [KafkaService],
     imports: [SlackModule, RedisCacheModule]
 })
 export class CommonModule {}
