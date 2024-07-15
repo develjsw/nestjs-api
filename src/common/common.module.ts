6bc1282545d1769ef94888fdc7e57d60f86d82ce
jsw <develjsw@gmail.com>
2024-07-15 19:36:44 +0900

    kafka test2

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,12 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { SlackModule } from './slack/slack.module';
 import { RedisCacheModule } from './cache/redis-cache.module';
-import { KafkaService } from './kafka/kafka.service';
 
 @Global()
 @Module({
-    providers: [KafkaService],
-    exports: [KafkaService],
+    providers: [],
+    exports: [],
     imports: [SlackModule, RedisCacheModule]
 })
 export class CommonModule {}
---
+++ src/kafka/kafka.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Kafka } from 'kafkajs';
+import { KafkaService } from './kafka.service';
+import { ProducerDto } from './dto/producer.dto';
+import { ConsumerDto } from './dto/consumer.dto';
+
+@Controller('kafka')
+export class KafkaController {
+    private kafka = new Kafka({
+        clientId: "peter's kafka",
+        brokers: ['localhost:9094']
+    });
+
+    constructor(private readonly kafkaService: KafkaService) {}
+
+    @Post('producer')
+    async producer(@Body(new ValidationPipe()) producerDto: ProducerDto) {
+        await this.kafkaService.processProducer(
+            producerDto.topic,
+            producerDto.messages
+        );
+    }
+
+    @Post('consumer')
+    async consumer(@Body(new ValidationPipe()) consumerDto: ConsumerDto) {
+        await this.kafkaService.processConsumer(consumerDto.topics);
+    }
+}
