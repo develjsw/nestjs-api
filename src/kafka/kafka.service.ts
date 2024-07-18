b1f585ef41939cafb481ced24d242bab4aa0303c
jsw <develjsw@gmail.com>
2024-07-18 23:04:23 +0900

    kafka test3

--- src/kafka/kafka.service.ts
+++ src/kafka/kafka.service.ts
@@ -1,5 +1,13 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { EachMessagePayload, Kafka, Partitioners, Producer } from 'kafkajs';
+import {
+    EachMessagePayload,
+    Kafka,
+    Partitioners,
+    Producer,
+    Consumer,
+    logLevel,
+    RecordMetadata
+} from 'kafkajs';
 
 @Injectable()
 export class KafkaService {
@@ -8,29 +16,40 @@ export class KafkaService {
     constructor() {
         this.kafka = new Kafka({
             clientId: 'property-test',
-            brokers: ['localhost:9094']
+            brokers: ['localhost:9094'],
+            logLevel: logLevel.DEBUG // DEBUG 로그 레벨 설정
         });
     }
 
     async processConsumer(topics: string[]): Promise<void> {
-        const consumerRes = this.kafka.consumer({
-            groupId: 'my-group'
+        const consumer: Consumer = this.kafka.consumer({
+            /*
+                groupId를 기준으로 새로운 메세지가 있는 경우에만 consumer 대상군 노출됨
+                따라서 groupId를 다르게 생성하면 새로운 groupId로 consumer 전체 대상군을 계속 소비 가능
+                EX)
+                    groupId: `my-group-${new Date().getTime()}` // 새로운 consumer group id 사용
+                    groupId: 'groupId: my-group-1721278528048' // consumer group id 고정값
+             */
+            groupId: 'groupId: my-group' // consumer group id 고정
         });
 
         try {
             console.log('Connecting consumer...');
-            await consumerRes.connect();
+            await consumer.connect();
             console.log('Consumer connected');
 
             console.log('Subscribing to topics:', topics);
-            await consumerRes.subscribe({ topics });
+            for (const topic of topics) {
+                await consumer.subscribe({ topic, fromBeginning: true });
+            }
             console.log('Subscription completed');
 
-            await consumerRes.run({
+            await consumer.run({
                 eachMessage: async (payload: EachMessagePayload) => {
                     await this.consumerCallback(payload);
                 }
             });
+
             console.log('Consumer is running');
         } catch (error) {
             console.error('Error in processConsumer:', error);
@@ -47,17 +66,17 @@ export class KafkaService {
             messages
         };
 
-        const producerRes: Producer = this.kafka.producer({
+        const producer: Producer = this.kafka.producer({
             createPartitioner: Partitioners.LegacyPartitioner // 기존 파티셔닝 방식 사용
         });
 
         try {
             console.log('Connecting producer...');
-            await producerRes.connect();
+            await producer.connect();
             console.log('Producer connected');
 
             console.log('Sending messages:', params);
-            const result = await producerRes.send(params);
+            const result: RecordMetadata[] = await producer.send(params);
             console.log('Producer result:', result);
         } catch (error) {
             console.error('Error in processProducer:', error);
@@ -66,12 +85,13 @@ export class KafkaService {
     }
 
     async consumerCallback(payload: EachMessagePayload): Promise<void> {
-        // 메세지 수신 콜백
+        // 메시지 수신 콜백
         console.log('Kafka message arrived');
-        console.log(
-            `Topic: ${
-                payload.topic
-            }, Message: ${payload.message.value.toString()}`
-        );
+        console.log(`
+            Topic: ${payload.topic},
+            Partition: ${payload.partition},
+            Offset: ${payload.message.offset},
+            Message: ${payload.message.value.toString()}
+        `);
     }
 }
