b0e1e19295c925e0e5818c32f1eec12bfb5ce6a8
jsw <develjsw@gmail.com>
2024-09-29 20:51:22 +0900

    주석 제거

--- src/common/cache/redis-cache.module.ts
+++ src/common/cache/redis-cache.module.ts
@@ -16,8 +16,6 @@ import { RedisCacheService } from './redis-cache.service';
                 socket: {
                     host: configService.get('database.redis.host'),
                     port: configService.get('database.redis.port')
-                    // TODO : module에서 설정한 ttl 값이 적용되지 않는 이슈가 존재하여 사용하는 곳에서 option으로 설정 중
-                    //ttl: configService.get('database.redis.defaultTTL')
                 }
             })
         })
