56686972af33a6cf27403ba6fc2efaaa9501b8ae
jsw <develjsw@gmail.com>
2024-10-08 14:58:02 +0900

    short 모듈 폴더 이동

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { SlackModule } from './slack/slack.module';
 import { RedisCacheModule } from './cache/redis-cache.module';
+import { ShortUrlModule } from './short-url/bitly/short-url.module';
 
 @Global()
 @Module({
     providers: [],
     exports: [],
-    imports: [SlackModule, RedisCacheModule]
+    imports: [SlackModule, RedisCacheModule, ShortUrlModule]
 })
 export class CommonModule {}
